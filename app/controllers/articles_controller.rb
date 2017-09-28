5842e185b910d47ec7051fb4b360b1ca52762621
Yasir <yasirch7722@gmail.com>
2017-09-28 22:06:11 +0500

    duplicate of the app

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+
+  def home
+
+  end
+
+  def about
+
+  end
+
+  def index
+
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'articles#home'
+  get 'about', to: 'articles#about'
+  resources :articles
+end
